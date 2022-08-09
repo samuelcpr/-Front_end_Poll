93e2015780bd82ea110b9bc1cc2b0369c9cf5cff
samuelcpr <samuelcpr464@gmail.com>
2022-08-09 15:55:06 -0300

    chore: configuration code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3001,
-	},
-});
+	export default defineConfig({
+		plugins: [
+			react({
+				babel: {
+					plugins: [
+						[
+							'babel-plugin-styled-components',
+							{
+								displayName: true,
+								fileName: false
+							}
+						]
+					]
+				}
+			})
+		]
+	})
